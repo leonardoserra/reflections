d72835f3a216e87aa63c65f5101a54edf54ceb07
leonardoserra <serra.leonardo92@gmail.com>
2026-03-21 18:43:05 +0100

    feat: page, added defaults to page number and body, and unique key

--- db/migrate/20260305233430_create_pages.rb
+++ db/migrate/20260305233430_create_pages.rb
@@ -1,11 +1,12 @@
 class CreatePages < ActiveRecord::Migration[8.1]
   def change
     create_table :pages do |t|
-      t.integer :number, null: false
-      t.text :body
+      t.integer :number, default: 1
+      t.text :body, default: ""
       t.belongs_to :pageable, polymorphic: true, null: false, index: true
 
       t.timestamps
     end
+    add_index :pages, [:number, :pageable_id], unique: true
   end
 end
