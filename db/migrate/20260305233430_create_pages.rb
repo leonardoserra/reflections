f45889d88286ea814224cef37e5a3c11ead8bc8f
leonardoserra <serra.leonardo92@gmail.com>
2026-07-03 00:03:05 +0200

    rubocop lint

--- db/migrate/20260305233430_create_pages.rb
+++ db/migrate/20260305233430_create_pages.rb
@@ -7,6 +7,6 @@ class CreatePages < ActiveRecord::Migration[8.1]
 
       t.timestamps
     end
-    add_index :pages, [:number, :pageable_id], unique: true
+    add_index :pages, [ :number, :pageable_id ], unique: true
   end
 end
